eaa6f9e2eaec1d5d192954521643b595f83a7935
Rutul Dave <rutul98@yahoo.com>
2015-07-17 17:19:15 -0700

    Methods to find and list mentioned users in a post body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  def mentions
+    @mentioned ||= begin
+                      regex = /@([\w]+)/
+                      body.scan(regex).flatten
+                   end
+  end
+
+  def mentioned_users
+    @mentioned_users ||= User.where(username: mentions)
+  end
 end
